385fc4666c40915e0f03b66f486fc919ac3d7d69
mattperkins <github@mattperkins.com>
2019-04-05 21:09:59 +0100

    prepare form

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <?php
-
+// get all package deps
 require './vendor/autoload.php';
 
+// display errors
 $app = new \Slim\App([
   'settings' => [
     'displayErrorDetails' => true,
   ]
 ]);
 
+// use container
 $container = $app->getContainer();
 
 // Register component on container
 $container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/src/views', [
-        // 'cache' => 'path/to/cache'
-        'cache' => false
-    ]);
+  $view = new \Slim\Views\Twig(__DIR__ . '/src/views', [
+      // 'cache' => 'path/to/cache'
+      'cache' => false
+  ]);
 
-    // Instantiate and add Slim specific extension
-    $router = $container->get('router');
-    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+  // Instantiate and add Slim specific extension
+  $router = $container->get('router');
+  $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+  $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
 
-    return $view;
+  return $view;
 };
 
-$app->get('/', function($request, $response) {
-  return $this->view->render($response, 'home.twig');
-})->setName('home');
-
-$app->get('/users', function($request, $response) {
-  $users = [
-    ['username' => 'Fred'],
-    ['username' => 'Sandy'],
-    ['username' => 'Bob'],
-  ];
-
-  return $this->view->render($response, 'users.twig', [
-    'lemon' => $users,
-  ]);
-})->setName('users.index');
+// routes
+require './src/routes/routes.php';
 
 $app->run();
